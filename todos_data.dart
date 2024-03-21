cdfda5f3e763737fb2f0a556350dcbb6f7df6adc
jisusDev <156206996+jisusDev@users.noreply.github.com>
2024-03-21 14:45:26 -0500

    <TodoList>

---
+++ todos_data.dart
@@ -0,0 +1,34 @@
+
+import 'dart:convert';
+
+List<TodosData> todosFromJson(String str) => List<TodosData>.from(json.decode(str).map((x) => TodosData.fromJson(x)));
+
+String todosToJson(List<TodosData> data) => json.encode(List<dynamic>.from(data.map((x) => x.toJson())));
+
+class TodosData {
+    final int userId;
+    final int id;
+    final String title;
+    late final bool completed;
+
+    TodosData({
+        required this.userId,
+        required this.id,
+        required this.title,
+        required this.completed,
+    });
+
+    factory TodosData.fromJson(Map<String, dynamic> json) => TodosData(
+        userId: json["userId"],
+        id: json["id"],
+        title: json["title"],
+        completed: json["completed"],
+    );
+
+    Map<String, dynamic> toJson() => {
+        "userId": userId,
+        "id": id,
+        "title": title,
+        "completed": completed,
+    };
+}
